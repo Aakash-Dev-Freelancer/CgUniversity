755afcfb442d5ad627b5fe5565a929977dbecb6d
Aakash <panwara925@gmail.com>
2024-05-08 08:00:29 +0530

    New Files

---
+++ CG_University/CG_International_University_App/models/admin_info.py
@@ -0,0 +1,272 @@
+from enum import Enum
+from dataclasses import dataclass
+from typing import Any, Optional, List, TypeVar, Type, Callable, cast
+from datetime import datetime
+import dateutil.parser
+
+
+T = TypeVar("T")
+EnumT = TypeVar("EnumT", bound=Enum)
+
+
+def from_int(x: Any) -> int:
+    assert isinstance(x, int) and not isinstance(x, bool)
+    return x
+
+
+def from_str(x: Any) -> str:
+    assert isinstance(x, str)
+    return x
+
+
+def to_enum(c: Type[EnumT], x: Any) -> EnumT:
+    assert isinstance(x, c)
+    return x.value
+
+
+def from_datetime(x: Any) -> datetime:
+    return dateutil.parser.parse(x)
+
+
+def from_none(x: Any) -> Any:
+    assert x is None
+    return x
+
+
+def from_union(fs, x):
+    for f in fs:
+        try:
+            return f(x)
+        except:
+            pass
+    assert False
+
+
+def from_list(f: Callable[[Any], T], x: Any) -> List[T]:
+    assert isinstance(x, list)
+    return [f(y) for y in x]
+
+
+def to_class(c: Type[T], x: Any) -> dict:
+    assert isinstance(x, c)
+    return cast(Any, x).to_dict()
+
+
+class Email(Enum):
+    EXAMPLE_GMAIL_COM = "example@gmail.com"
+    PANWARA925_GMAIL_COM = "panwara925@gmail.com"
+    W324_E_GMAIL_COM = "w324e@gmail.com"
+    WE_GMAIL_COM = "we@gmail.com"
+
+
+@dataclass
+class AdminInfoClass:
+    id: int
+    full_name: str
+    contact_no: str
+    email: Email
+    username: str
+    password: int
+
+    @staticmethod
+    def from_dict(obj: Any) -> 'AdminInfoClass':
+        assert isinstance(obj, dict)
+        id = from_int(obj.get("id"))
+        full_name = from_str(obj.get("full_name"))
+        contact_no = from_str(obj.get("contact_no"))
+        email = Email(obj.get("email"))
+        username = from_str(obj.get("username"))
+        password = int(from_str(obj.get("password")))
+        return AdminInfoClass(id, full_name, contact_no, email, username, password)
+
+    def to_dict(self) -> dict:
+        result: dict = {}
+        result["id"] = from_int(self.id)
+        result["full_name"] = from_str(self.full_name)
+        result["contact_no"] = from_str(self.contact_no)
+        result["email"] = to_enum(Email, self.email)
+        result["username"] = from_str(self.username)
+        result["password"] = from_str(str(self.password))
+        return result
+
+
+class Address(Enum):
+    THE_35_NAGAR = "35, nagar"
+    THE_42_NAGAR = "42, nagar"
+    THE_46_MOHAN_NAGAR = "46, Mohan Nagar"
+
+
+class Category(Enum):
+    ADFKJ = "adfkj"
+    SC = "SC"
+    WE = "WE"
+
+
+class Gender(Enum):
+    FEMALE = "Female"
+    MALE = "Male"
+
+
+class MobileNumber(Enum):
+    ADF = "adf"
+    ADFED = "adfed"
+    THE_0700064 = "0700064"
+    THE_07000649593 = "07000649593"
+
+
+class MotherName(Enum):
+    HEMLATA_PANWAR = "Hemlata Panwar"
+    MOTHER = "Mother"
+
+
+@dataclass
+class Student:
+    enrollment_no: str
+    password: int
+    profile_pic: int
+    full_name: str
+    father_name: str
+    mother_name: MotherName
+    gender: Gender
+    date_of_birth: datetime
+    category: Category
+    email: Email
+    mobile_number: MobileNumber
+    address: Address
+
+    @staticmethod
+    def from_dict(obj: Any) -> 'Student':
+        assert isinstance(obj, dict)
+        enrollment_no = from_str(obj.get("enrollment_no"))
+        password = int(from_str(obj.get("password")))
+        profile_pic = int(from_str(obj.get("profile_pic")))
+        full_name = from_str(obj.get("full_name"))
+        father_name = from_str(obj.get("father_name"))
+        mother_name = from_str(obj.get("mother_name"))
+        gender = from_str(obj.get("gender"))
+        date_of_birth = from_datetime(obj.get("date_of_birth"))
+        category = from_str(obj.get("category"))
+        email = from_str(obj.get("email"))
+        mobile_number = from_str(obj.get("mobile_number"))
+        address = from_str(obj.get("address"))
+        return Student(enrollment_no, password, profile_pic, full_name, father_name, mother_name, gender, date_of_birth, category, email, mobile_number, address)
+
+    def to_dict(self) -> dict:
+        result: dict = {}
+        result["enrollment_no"] = from_str(self.enrollment_no)
+        result["password"] = from_str(str(self.password))
+        result["profile_pic"] = from_str(str(self.profile_pic))
+        result["full_name"] = from_str(self.full_name)
+        result["father_name"] = from_str(self.father_name)
+        result["mother_name"] = to_enum(MotherName, self.mother_name)
+        result["gender"] = to_enum(Gender, self.gender)
+        result["date_of_birth"] = self.date_of_birth.isoformat()
+        result["category"] = to_enum(Category, self.category)
+        result["email"] = to_enum(Email, self.email)
+        result["mobile_number"] = to_enum(MobileNumber, self.mobile_number)
+        result["address"] = to_enum(Address, self.address)
+        return result
+
+
+@dataclass
+class StudentsDatum:
+    student_enrollment_no: str
+    student_provision: str
+    student_admit_card: Optional[str] = None
+    student_affidevit: Optional[str] = None
+    student_photo: Optional[str] = None
+    student_migrations: Optional[str] = None
+
+    @staticmethod
+    def from_dict(obj: Any) -> 'StudentsDatum':
+        assert isinstance(obj, dict)
+        student_enrollment_no = from_str(obj.get("student_enrollment_no"))
+        student_provision = from_str(obj.get("student_provision"))
+        student_admit_card = from_union([from_none, from_str], obj.get("student_admit_card"))
+        student_affidevit = from_union([from_none, from_str], obj.get("student_affidevit"))
+        student_photo = from_union([from_none, from_str], obj.get("student_photo"))
+        student_migrations = from_union([from_none, from_str], obj.get("student_migrations"))
+        return StudentsDatum(student_enrollment_no, student_provision, student_admit_card, student_affidevit, student_photo, student_migrations)
+
+    def to_dict(self) -> dict:
+        result: dict = {}
+        result["student_enrollment_no"] = from_str(self.student_enrollment_no)
+        result["student_provision"] = from_str(self.student_provision)
+        result["student_admit_card"] = from_union([from_none, from_str], self.student_admit_card)
+        result["student_affidevit"] = from_union([from_none, from_str], self.student_affidevit)
+        result["student_photo"] = from_union([from_none, from_str], self.student_photo)
+        result["student_migrations"] = from_union([from_none, from_str], self.student_migrations)
+        return result
+
+
+@dataclass
+class StudentsMarksheet:
+    id: int
+    student_enrollment_no: str
+    session: str
+    semester: int
+    sgpa: str
+    status: str
+    result: str
+    file: str
+
+    @staticmethod
+    def from_dict(obj: Any) -> 'StudentsMarksheet':
+        assert isinstance(obj, dict)
+        id = from_int(obj.get("id"))
+        student_enrollment_no = from_str(obj.get("student_enrollment_no"))
+        session = from_str(obj.get("session"))
+        semester = int(from_str(obj.get("semester")))
+        sgpa = from_str(obj.get("sgpa"))
+        status = from_str(obj.get("status"))
+        result = from_str(obj.get("result"))
+        file = from_str(obj.get("file"))
+        return StudentsMarksheet(id, student_enrollment_no, session, semester, sgpa, status, result, file)
+
+    def to_dict(self) -> dict:
+        result: dict = {}
+        result["id"] = from_int(self.id)
+        result["student_enrollment_no"] = from_str(self.student_enrollment_no)
+        result["session"] = from_str(self.session)
+        result["semester"] = from_str(str(self.semester))
+        result["sgpa"] = from_str(self.sgpa)
+        result["status"] = from_str(self.status)
+        result["result"] = from_str(self.result)
+        result["file"] = from_str(self.file)
+        return result
+
+
+@dataclass
+class AdminInfo:
+    success: str
+    admin_info: AdminInfoClass
+    students: List[Student]
+    students_data: List[StudentsDatum]
+    students_marksheets: List[StudentsMarksheet]
+
+    @staticmethod
+    def from_dict(obj: Any) -> 'AdminInfo':
+        assert isinstance(obj, dict)
+        success = from_str(obj.get("success"))
+        admin_info = AdminInfoClass.from_dict(obj.get("admin_info"))
+        students = from_list(Student.from_dict, obj.get("students"))
+        students_data = from_list(StudentsDatum.from_dict, obj.get("students_data"))
+        students_marksheets = from_list(StudentsMarksheet.from_dict, obj.get("students_marksheets"))
+        return AdminInfo(success, admin_info, students, students_data, students_marksheets)
+
+    def to_dict(self) -> dict:
+        result: dict = {}
+        result["success"] = from_str(self.success)
+        result["admin_info"] = to_class(AdminInfoClass, self.admin_info)
+        result["students"] = from_list(lambda x: to_class(Student, x), self.students)
+        result["students_data"] = from_list(lambda x: to_class(StudentsDatum, x), self.students_data)
+        result["students_marksheets"] = from_list(lambda x: to_class(StudentsMarksheet, x), self.students_marksheets)
+        return result
+
+
+def admin_info_from_dict(s: Any) -> AdminInfo:
+    return AdminInfo.from_dict(s)
+
+
+def admin_info_to_dict(x: AdminInfo) -> Any:
+    return to_class(AdminInfo, x)
